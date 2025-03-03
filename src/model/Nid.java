7544b45ac3d8a18796e6f1389ece4b70cd20834d
esther-deba <debaesther72@gmail.com>
2025-03-03 16:29:21 +0100

    nid

---
+++ src/model/Nid.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Nid {
+    private int cptRessources;
+    private Fourmi[] fourmis;
+
+    public Nid(int cptRessources, Fourmi[] fourmis) {
+        this.cptRessources = cptRessources;
+        this.fourmis = fourmis;
+    }
+}
