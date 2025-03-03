cf70ea8546ddac1f74aad73cb9ca92f18019b417
esther-deba <debaesther72@gmail.com>
2025-03-03 16:55:30 +0100

    arrayFourmis

--- src/model/Nid.java
+++ src/model/Nid.java
@@ -1,14 +1,16 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Nid extends ObjetFixe {
     private int cptRessources;
-    private Fourmi[] fourmis;
+    private ArrayList<Fourmi> fourmis;
     private int x;
     private int y;
 
     public Nid(int cptRessources, Fourmi[] fourmis, int x, int y) {
         super(x, y);
         this.cptRessources = cptRessources;
-        this.fourmis = fourmis;
+        this.fourmis = new ArrayList<Fourmi>();
     }
 }
