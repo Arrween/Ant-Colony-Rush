d330c9d828ce8827b4099b419d51de37b6afefcd
esther-deba <debaesther72@gmail.com>
2025-03-03 15:49:23 +0100

    ObjetFixe

---
+++ src/ObjetFixe.java
@@ -0,0 +1,17 @@
+public class ObjetFixe {
+    private int x;
+    private int y;
+
+    public ObjetFixe(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    int getx() {
+        return x;
+    }
+
+    int gety() {
+        return y;
+    }
+}
