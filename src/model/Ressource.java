a611ec119a03838a584b16b8c2b7e2bb1b7f2062
esther-deba <debaesther72@gmail.com>
2025-03-03 16:46:48 +0100

    nbrFourmis

--- src/model/Ressource.java
+++ src/model/Ressource.java
@@ -3,10 +3,12 @@ package model;
 public class Ressource {
     private int poids;
     private int valeur_nutritive;
+    private int nbr_fourmis;
 
-    public Ressource(int poids, int valeur_nutritive) {
+    public Ressource(int poids, int valeur_nutritive, int nbr_fourmis) {
         this.poids = poids;
         this.valeur_nutritive = valeur_nutritive;
+        this.nbr_fourmis = nbr_fourmis;
     }
 
     public int getPoids() {
@@ -15,5 +17,9 @@ public class Ressource {
 
     public int getValeurNutritive() {
         return valeur_nutritive;
+    }
+
+    public int getNbrFourmis() {
+        return nbr_fourmis;
     }
 }
