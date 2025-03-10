d8b7d1b9b4ba23b11c9e3eb9a2d9f4a92caf558f
Ania Garoui <ania.garoui.etu@univ-lemans.fr>
2025-03-10 23:17:02 +0100

    Génération du nid de manière aléatoire dans Affichage

--- src/view/Affichage.java
+++ src/view/Affichage.java
@@ -2,28 +2,37 @@ package view;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
-
+import java.awt.Image;
+import java.util.Random;
 import javax.swing.*;
-
 import model.Terrain;
 
 public class Affichage extends JPanel {
+    private Image image;
+    private int x;
+    private int y;
+
     public static final double RATIO_X = 1.0;
     public static final double RATIO_Y = 1.0;
     public static final Dimension PANELSIZE = new Dimension((int) (Terrain.LARGEUR * RATIO_X),
-            (int) (Terrain.HAUTEUR * RATIO_Y));
+                                                            (int) (Terrain.HAUTEUR * RATIO_Y));
     private Terrain t;
  
     public Affichage(Terrain ter) {
-        super();
         this.setPreferredSize(PANELSIZE);
         this.t = ter;
+
+        ImageIcon icon = new ImageIcon("ressources/nid.png");
+        image = icon.getImage();
+
+        Random random = new Random();
+        this.x = random.nextInt(Terrain.LARGEUR);
+        this.y = random.nextInt(Terrain.HAUTEUR);
     }
 
     @Override
-    public void paint(Graphics g) {
-        super.paint(g);
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(image, x, y, 40, 40, this);
     }
-
 }
-
