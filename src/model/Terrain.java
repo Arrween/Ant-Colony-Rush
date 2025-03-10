12bdab34d512758581bce33b57a7b636cc1b6e15
Johan MARTIN-BORRET <johan.martin-borret@etu-upsaclay.fr>
2025-03-10 15:13:09 +0100

    suppression grille

--- src/model/Terrain.java
+++ src/model/Terrain.java
@@ -1,5 +1,9 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.awt.Point;
+
 public class Terrain {
     private static final int TAILLE_PIXEL = 20;
     public static final int NB_PIXELS_LAR = 60;
@@ -7,13 +11,10 @@ public class Terrain {
     public static final int LARGEUR = TAILLE_PIXEL * NB_PIXELS_LAR;
     public static final int HAUTEUR = TAILLE_PIXEL * NB_PIXELS_HAU;
 
-    ObjetFixe[][] grid;
+    //ObjetFixe[][] grid;
+    ArrayList<ObjetFixe> elts;
 
     public Terrain() {
-        grid = new ObjetFixe[NB_PIXELS_HAU][NB_PIXELS_LAR];
-    }
-
-    public ObjetFixe getObjetFixe(int x, int y) {
-        return grid[y / TAILLE_PIXEL][x / TAILLE_PIXEL];
+        elts = new ArrayList<ObjetFixe>();
     }
 }
