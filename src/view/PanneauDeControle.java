cc81b3f104a512360b997951499687eeb4b6b11d
esther-deba <debaesther72@gmail.com>
2025-03-17 15:50:44 +0100

    Changement de constructeu PanneauDeControle

--- src/view/PanneauDeControle.java
+++ src/view/PanneauDeControle.java
@@ -4,21 +4,23 @@ import java.awt.*;
 import java.util.List;
 import javax.swing.*;
 import model.Fourmi;
+import model.Nid;
 
 public class PanneauDeControle extends JPanel {
 
+    private Nid nid;
     private JLabel lblScore; // Label pour afficher la valeur nutritive des ressources
     private JPanel pnlFourmis; // Panel pour afficher les fourmis
 
-    public PanneauDeControle(int score, List<Fourmi> fourmis) {
+    public PanneauDeControle(Nid nid) {
+        this.nid = nid;
         setLayout(new BorderLayout());
         setPreferredSize(new Dimension(300, 500)); // Taille de la fenêtre
 
         // NORTH : Affichage de la valeur nutritive des ressources + bouton fermeture
 
         JPanel headerPanel = new JPanel(new BorderLayout());
-        lblScore = new JLabel("SCORE : " + score,
-                SwingConstants.CENTER);
+        lblScore = new JLabel("SCORE : " + nid.getScore(), SwingConstants.CENTER);
         lblScore.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         headerPanel.add(lblScore, BorderLayout.CENTER);
 
@@ -42,8 +44,8 @@ public class PanneauDeControle extends JPanel {
         // CENTRE : Liste des fourmis
 
         pnlFourmis = new JPanel(new GridLayout(0, 1, 5, 5));
-        for (int i = 0; i < fourmis.size(); i++) {
-            FourmiButton fourmiButton = new FourmiButton(fourmis.get(i), i + 1);
+        for (int i = 0; i < nid.getFourmis().size(); i++) {
+            FourmiButton fourmiButton = new FourmiButton(nid.getFourmis().get(i), i + 1);
             fourmiButton.setPreferredSize(new Dimension(250, 40));
             pnlFourmis.add(fourmiButton);
         }
@@ -56,6 +58,12 @@ public class PanneauDeControle extends JPanel {
 
         JButton btnAjouterFourmi = new JButton("Ajouter une fourmi");
         btnAjouterFourmi.setPreferredSize(new Dimension(280, 40));
+        btnAjouterFourmi.addActionListener(e -> {
+            // Appel au modèle pour ajouter une fourmi
+            nid.ajouterFourmi();
+            // Met à jour la liste des fourmis dans l'affichage
+            updateFourmis(nid.getFourmis());
+        });
         JPanel southPanel = new JPanel();
         southPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         southPanel.add(btnAjouterFourmi);
@@ -63,7 +71,7 @@ public class PanneauDeControle extends JPanel {
     }
 
     // Mise à jour dynamique
-    public void updateValeurNutritiveRessources(int score) {
+    public void updateScore(int score) {
         lblScore.setText("SCORE : " + score);
     }
 
