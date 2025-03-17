4679d6d830e490e9497caef3c31d4d02b67e847e
Johan MARTIN-BORRET <johan.martin-borret@etu-upsaclay.fr>
2025-03-17 14:52:43 +0100

    maj main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,19 @@
 package main;
 
 import javax.swing.SwingUtilities;
+
+import model.GestionnaireEnergie;
+import model.Terrain;
 import view.GameFrame;
 
 public class Main {
     public static void main(String[] args) {
+        Terrain t = new Terrain();
+        GestionnaireEnergie ge = new GestionnaireEnergie(t);
+        ge.start();
 
         SwingUtilities.invokeLater(() -> {
-            new GameFrame();
+            new GameFrame(t);
         });
     }
 }
