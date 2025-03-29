3de371a916bcc36aae13f97837727389d8378ff0
Johan MARTIN-BORRET <johan.martin-borret@etu-upsaclay.fr>
2025-03-29 15:58:09 +0100

    transports

---
+++ src/model/DeplacementSimple.java
@@ -0,0 +1,35 @@
+package model;
+
+public class DeplacementSimple extends Deplacement{
+    private Fourmi f;
+
+    public DeplacementSimple(Terrain t, Fourmi fourmi, ObjetFixe dest, ObjetFixe depart) {
+        super(t, dest, depart);
+        this.f = fourmi;
+    }
+
+    public void decrEnergieFourmi() {
+        f.decrEnergie();
+    }
+
+    protected void updateVitesse() {
+        vitesse = f.getVitesse();
+    }
+
+    public void avancer() {
+        System.out.println("Fourmi " + f.getId() + " en déplacement");
+        //algo de déplacmenent////
+        
+        // Mise à jour de la vitesse, position, direction et animation de la fourmi
+        updateVitesse();
+        updatePosition();
+        updateDirection();
+        animationFourmi.updateFrame();
+
+        //si déplacment fini ajouter la fourmi à l'objet destination et marquer comme fini
+        if (!isDone && currentX == destX && currentY == destY) {
+            dest.ajouterFourmi(f);
+            isDone = true;
+        }
+    }
+}
