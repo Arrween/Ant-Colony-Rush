520e56bf6850f37c16cf100eab2737b221e8e1a4
esther-deba <debaesther72@gmail.com>
2025-03-31 14:58:57 +0200

    merge_esther

--- tests/tests/DirectoryTest.java
+++ tests/tests/DirectoryTest.java
@@ -1,16 +1,20 @@
 package tests;
 
-import java.awt.Image;
-import javax.swing.ImageIcon;
-
 public class DirectoryTest {
     public static void main(String[] args) {
-        String currentDir = System.getProperty("user.dir");
-        System.out.println("Répertoire courant : " + currentDir);
-        // Afficher une image aux points (0, 0)
-        Image img = new ImageIcon(currentDir + "/ressources/Not/nid.png").getImage();
-        // Afficher img dans une fenêtre 
-        
+        // String currentDir = System.getProperty("user.dir");
+        // System.out.println("Répertoire courant : " + currentDir);
+        // // Afficher une image aux points (0, 0)
+        // Image img = new ImageIcon(currentDir + "/ressources/Not/nid.png").getImage();
+        // // Afficher img dans une fenêtre
+
+        // URL url = Nid.class.getResource("/ressources/Not/nid.png");
+        // System.out.println("URL de l'image : " + url);
+
+        // if (url == null) {
+        // System.err.println("L'image nid.png est introuvable !");
+        // }
+        // ImageIcon icon = new ImageIcon(url);
 
     }
 }
