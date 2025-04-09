0863dbb0fccd887e88f59eca1db0ab1d956a0c47
amineouat <ouatmani.amine@gmail.com>
2025-04-09 10:51:17 -0400

    Generation Ress

--- src/controller/StartMenuController.java
+++ src/controller/StartMenuController.java
@@ -7,6 +7,7 @@ import javax.swing.*;
 import model.GestionnaireCrapaud;
 import model.GestionnaireDeplacement;
 import model.GestionnaireEnergie;
+import model.GestionnaireRessources;
 import model.Score;
 import model.Terrain;
 import view.DifficultePanel;
@@ -72,9 +73,10 @@ public class StartMenuController {
         GestionnaireEnergie gestionnaireEnergie = new GestionnaireEnergie(terrain);
         GestionnaireDeplacement gestionnaireDeplacement = new GestionnaireDeplacement(terrain, terrainPanel);
         GestionnaireCrapaud gestionnaireCrapaud = new GestionnaireCrapaud(terrain);
+        GestionnaireRessources gestionnaireRessources = new GestionnaireRessources(terrain);
 
         Score score = new Score(terrain.getNid());
-        
+
         // thread de gestion du score
         GestionScore gestionScore = new GestionScore(score, terrain.getNid());
         gestionScore.start();
@@ -89,6 +91,7 @@ public class StartMenuController {
         gestionnaireEnergie.start();
         gestionnaireDeplacement.start();
         gestionnaireCrapaud.start();
+        gestionnaireRessources.start();
         redessinJeuFrame.start();
 
         // Fermer la fenêtre du menu de démarrage
